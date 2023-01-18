31a41833ef88ec716ce17cdf62766b0beb2395ea
root <root@HonorLaptop.localdomain>
2023-01-18 11:06:44 +0100

    fix execution type issue

--- Neo4jConnector/Neo4jConnector.py
+++ Neo4jConnector/Neo4jConnector.py
@@ -3,6 +3,10 @@ import logging
 from neo4j import GraphDatabase, Neo4jDriver
 
 class Neo4jConnector:
+
+    READ = 0
+    WRITE = 1
+
     def __init__(self, uri, user, password, database):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
         self.database = database
@@ -10,10 +14,15 @@ class Neo4jConnector:
     def close(self):
         self.driver.close()
 
-    def run(self, query, **kargs):
+    def run(self, execution, query, **kargs):
         with self.driver.session(database=self.database) as session:
-            result = session.execute_read(self.gen_static_method(query), **kargs)
-            return result
+            exec_function = {self.WRITE:session.execute_write, self.READ:session.execute_read}
+
+            if execution in exec_function.keys():
+                return exec_function[execution](self.gen_static_method(query), **kargs)
+            else:
+                logging.error('Invalid Execution')
+                return []
 
     @staticmethod   
     def staticmethod(tx, query, **kargs):
@@ -34,9 +43,11 @@ if __name__ == '__main__':
     load_dotenv()
 
     a = Neo4jConnector(os.environ['NEO4J_URI'], os.environ['NEO4J_USER'], os.environ['NEO4J_PASSWORD'], os.environ['NEO4J_DATABASE'])
-    b = a.run("MATCH (n:Person {name:$name})-[:Poto]->(p:Person {name:'Elie'}) RETURN n, p", name="Alexis")
+    c = a.run(a.WRITE, "CREATE (n:Person {name:$name})", name="Michel")
+    b = a.run(a.READ, "MATCH (n:Person {name:$name})-[:Poto]->(p:Person {name:'Elie'}) RETURN n, p", name="Alexis")
 
+    print(c)
     for i in b:
         print(i)
-        
+
     a.close()
